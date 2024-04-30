cbc2f6a11434d9557659c9789e331df9c2cdf31a
Talgat-qypshaq <talgat.manglayev@gmail.com>
2024-05-01 01:10:54 +0500

    added python implementation; not fixed yet; optimized;

--- Python/main.py
+++ Python/main.py
@@ -5,18 +5,13 @@ BLOCKS = 8
 THREADS = 512
 VARIANT = 1
 
-def initializeArray(blocks_times_threads):
-    return [1] * blocks_times_threads
-
-def checkResults(array):
-    sum = 0
-    for element in array:
-        sum += element
-    return sum
-
-def callReduction(dev_a, dev_b):
+def callReduction(a, b):
+    dev_a = cuda.to_device(a)
+    dev_b = cuda.to_device(b)
     if VARIANT == 1:
         reduction_1[BLOCKS, THREADS](dev_a, dev_b)
+    sum = dev_b.copy_to_host()
+    return sum
 
 @cuda.jit
 def reduction_1(g_idata, g_odata):
@@ -24,20 +19,13 @@ def reduction_1(g_idata, g_odata):
     i = cuda.grid(1)
     sdata[cuda.threadIdx.x] = g_idata[i]
     cuda.syncthreads()
+    #do reduction in shared mem
     s = 1
-    
     while s < cuda.blockDim.x:
         if cuda.threadIdx.x % (2*s) == 0:
             sdata[cuda.threadIdx.x] += sdata[cuda.threadIdx.x + s]
+        cuda.syncthreads()
         s*=2
-        cuda.syncthreads()
-    #do reduction in shared mem
-    '''
-    for s in range(1, cuda.blockDim.x, s*2):
-        if cuda.threadIdx.x % (2*s) == 0:
-            sdata[cuda.threadIdx.x] += sdata[cuda.threadIdx.x + s]
-        cuda.syncthreads()
-    '''
     # write result for this block to global mem
     if cuda.threadIdx.x == 0:
         g_odata[cuda.blockIdx.x] = sdata[0]
@@ -46,29 +34,16 @@ def reduction_1(g_idata, g_odata):
     while s < cuda.blockDim.x:
         if cuda.threadIdx.x % (2*s) == 0:
             sdata[cuda.threadIdx.x] += g_odata[cuda.threadIdx.x + s]
-        s*=2
         cuda.syncthreads()
-    '''
-    for s in range(1, cuda.blockDim.x, s*2):
-        if cuda.threadIdx.x % (2*s) == 0:
-            g_odata[cuda.threadIdx.x] += g_odata[cuda.threadIdx.x + s];
-        cuda.syncthreads();
-    '''
+        s*=2
     #write result for this block to global mem
     if cuda.threadIdx.x == 0:
         g_odata[cuda.blockIdx.x] = g_odata[0];
 
 if __name__ == "__main__":
-    a = initializeArray(BLOCKS*THREADS)
-    b = initializeArray(BLOCKS*THREADS)
-
-    dev_a = cuda.to_device(a)
-    dev_b = cuda.to_device(b)
-
-    callReduction(dev_a, dev_b)
-
-    b = dev_b.copy_to_host()
-    print("GPU RESULTS: b =", b[0])
-
-    sum = checkResults(a)
-    print("CPU RESULTS: sum =", sum)
+    a = np.ones(BLOCKS*THREADS, dtype=np.int32)
+    b = np.ones(1, dtype=np.int32)
+    b = callReduction(a, b)
+    print("GPU RESULTS: ", b[0])
+    sum = np.sum(a)
+    print("CPU RESULTS: ", sum)
