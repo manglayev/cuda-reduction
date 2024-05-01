efe445e4b9085e77008067587aae9d4352c5c852
Talgat-qypshaq <talgat.manglayev@gmail.com>
2024-05-01 19:23:17 +0500

    fixed Python version Variant 1 for array size 4096

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numba import cuda, int32
+import numba
+from numba import cuda
 
 BLOCKS = 8
 THREADS = 512
@@ -13,32 +14,36 @@ def callReduction(a, b):
     sum = dev_b.copy_to_host()
     return sum
 
-@cuda.jit
+@numba.cuda.jit
 def reduction_1(g_idata, g_odata):
-    sdata = cuda.shared.array(shape=THREADS, dtype=int32)
-    i = cuda.grid(1)
-    sdata[cuda.threadIdx.x] = g_idata[i]
-    cuda.syncthreads()
-    #do reduction in shared mem
+    sdata = numba.cuda.shared.array(shape=THREADS, dtype=numba.int32)
+    i = numba.cuda.grid(1)
+    sdata[numba.cuda.threadIdx.x] = g_idata[i]
+    numba.cuda.syncthreads()
+    #do reduction in shared memory
     s = 1
-    while s < cuda.blockDim.x:
-        if cuda.threadIdx.x % (2*s) == 0:
-            sdata[cuda.threadIdx.x] += sdata[cuda.threadIdx.x + s]
-        cuda.syncthreads()
+    while s < numba.cuda.blockDim.x:
+        if numba.cuda.threadIdx.x % (2*s) == 0:
+            sdata[numba.cuda.threadIdx.x] += sdata[numba.cuda.threadIdx.x + s]
+        numba.cuda.syncthreads()
         s*=2
-    # write result for this block to global mem
-    if cuda.threadIdx.x == 0:
-        g_odata[cuda.blockIdx.x] = sdata[0]
+    numba.cuda.syncthreads()
+    # write result for this block to global memory
+    if numba.cuda.threadIdx.x == 0:
+        g_odata[numba.cuda.blockIdx.x] = sdata[0]
     #implement second reduction for the summed array
+    numba.cuda.syncthreads()
     s = 1
-    while s < cuda.blockDim.x:
-        if cuda.threadIdx.x % (2*s) == 0:
-            sdata[cuda.threadIdx.x] += g_odata[cuda.threadIdx.x + s]
-        cuda.syncthreads()
+    while s < numba.cuda.blockDim.x:
+        if numba.cuda.threadIdx.x % (2*s) == 0:
+            g_odata[cuda.threadIdx.x] += g_odata[numba.cuda.threadIdx.x + s]
+        numba.cuda.syncthreads()
         s*=2
-    #write result for this block to global mem
-    if cuda.threadIdx.x == 0:
-        g_odata[cuda.blockIdx.x] = g_odata[0];
+    numba.cuda.syncthreads()
+    #write result for this block to global memory
+    if numba.cuda.threadIdx.x == 0:
+        g_odata[numba.cuda.blockIdx.x] = g_odata[0];
+    numba.cuda.syncthreads()
 
 if __name__ == "__main__":
     a = np.ones(BLOCKS*THREADS, dtype=np.int32)
