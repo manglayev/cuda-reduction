5bfc6d8cce0e38df080f0b805c9dc2d7a203d583
Talgat-qypshaq <talgat.manglayev@gmail.com>
2024-05-10 16:28:14 +0500

    added CUDA device capabilities for C++ version

--- C++/reduction.cu
+++ C++/reduction.cu
@@ -66,7 +66,23 @@ int checkResults(int *a)
 void wrapper()
 {
   printf("STAGE 3 WRAPPER START\n");
-
+  cudaDeviceProp device;
+  //cudaGetDeviceCount(&count);
+  cudaGetDeviceProperties(&device, 0);
+  printf("  --- General information for device START ---\n");
+  printf("Name: %s;\n", device.name);
+  printf("Compute capability: %d.%d\n", device.major, device.minor);
+  printf("Clock rate: %d\n", device.clockRate);
+  printf("Total global memory: %ld\n", device.totalGlobalMem);
+  printf("Total constant memory: %ld\n", device.totalConstMem);
+  printf("Multiprocessor count: %d\n", device.multiProcessorCount);
+  printf("Shared memory per block: %ld\n", device.sharedMemPerBlock);
+  printf("Registers per block: %d\n", device.regsPerBlock);
+  printf("Threads in warp: %d\n", device.warpSize);
+  printf("Max threads Per Block: %d\n", device.maxThreadsPerBlock);
+  printf("Max thread dimensions: (%d, %d, %d)\n", device.maxThreadsDim[0], device.maxThreadsDim[1], device.maxThreadsDim[2]);
+  printf("Max grid dimensions: (%d, %d, %d)\n", device.maxGridSize[0], device.maxGridSize[1], device.maxGridSize[2]);
+  printf("  --- General information for device END ---\n");
   cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
